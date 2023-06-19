e9a239b2aa5320c1f2695675b43d8ceed18b8cf0
NIKHILESH KUMAR REDDY ANAM <nikhilesh@NIKHILESHs-MacBook-Air.local>
2023-06-19 20:26:29 +0530

    merge sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -10,10 +10,10 @@
 void merge(std::vector<int>&vec, std::vector<int>&left_vec, std::vector<int>&right_vec)
 {
     /*
-        The function takes three input parameters: the original vector arr, 
+        This merge function takes three input parameters: the original vector vec, 
         and the two sub-vectors left and right that are to be merged.
 
-        It initializes three index variables: i for left, j for right, and k for arr. These variables 
+        It initializes three index variables: i for left, j for right, and k for vec. These variables 
         keep track of the current positions within the sub-vectors and the merged vector, respectively.
 
         The function then enters a loop that continues until all elements from either 
@@ -21,26 +21,26 @@ void merge(std::vector<int>&vec, std::vector<int>&left_vec, std::vector<int>&rig
         elements at indices i and j in left and right, respectively.
 
         If the element at left[i] is less than or equal to the element at right[j], 
-        it means that the element from left should be placed in arr[k] because it is smaller or equal. 
-        So, the element is copied from left[i] to arr[k], and both i and k are incremented.
+        it means that the element from left should be placed in vec[k] because it is smaller or equal. 
+        So, the element is copied from left[i] to vec[k], and both i and k are incremented.
 
         If the element at left[i] is greater than the element at right[j], 
-        it means that the element from right should be placed in arr[k] because it is smaller. 
-        So, the element is copied from right[j] to arr[k], and both j and k are incremented.
+        it means that the element from right should be placed in vec[k] because it is smaller. 
+        So, the element is copied from right[j] to vec[k], and both j and k are incremented.
 
-        After copying an element to arr, the loop continues to the next iteration, 
+        After copying an element to vec, the loop continues to the next iteration, 
         comparing and copying the next elements until either left or right is fully processed.
 
         Once the loop finishes, there might still be remaining elements in either left or right. 
-        The function then enters two separate loops to copy any remaining elements to arr.
+        The function then enters two separate loops to copy any remaining elements to vec.
 
-        The first loop copies any remaining elements from left to arr, starting from index i 
+        The first loop copies any remaining elements from left to vec, starting from index i 
         and continuing until the end of left. It increments both i and k after each copy.
 
-        The second loop copies any remaining elements from right to arr, starting from index j 
+        The second loop copies any remaining elements from right to vec, starting from index j 
         and continuing until the end of right. It increments both j and k after each copy.
 
-        Finally, the merged and sorted elements are now stored in the original vector arr, 
+        Finally, the merged and sorted elements are now stored in the original vector vec, 
         and the function returns.
     
     */
@@ -97,8 +97,8 @@ void merge_sort(std::vector<int>&vec)
 
     /* 
         Halving the vector vec. Create two new vectors: left and right. 
-        left is a copy of the elements from the beginning of arr up to the middle index 
-        while right is a copy of the elements from the middle index to the end of arr.
+        left is a copy of the elements from the beginning of vec up to the middle index 
+        while right is a copy of the elements from the middle index to the end of vec.
     */
     std::vector<int> left_vec(vec.begin(), vec.begin() + mid);
     std::vector<int> right_vec(vec.begin() + mid, vec.end());
