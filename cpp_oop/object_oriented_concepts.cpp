c721c1013eeb5e10455dc7fa46ffd4344c23a7f1
NIKHILESH KUMAR REDDY ANAM <nikhilesh@NIKHILESHs-MacBook-Air.local>
2023-06-24 12:12:08 +0530

    C++ OOP concepts

--- cpp_oop/object_oriented_concepts.cpp
+++ cpp_oop/object_oriented_concepts.cpp
@@ -16,7 +16,10 @@ public:
     Animal(const std::string& _name) : name(_name) {}
 
     // Pure virtual function for abstraction
-    virtual void makeSound() const = 0;
+    // Virtual function for overriding
+    virtual void makeSound() const {
+        std::cout << "Animal makes a sound." << std::endl;
+    }
 
     virtual ~Animal() {
         std::cout << "Animal destructor called for " << name << "." << std::endl;
@@ -25,14 +28,10 @@ public:
 
 // Derived class
 class Dog : public Animal {
-    /*
-        Inheritance Relationship: The Dog class inherits from the Animal class using the 
-        public access specifier. This relationship is established through the line class Dog : public Animal.
-    */
 public:
     Dog(const std::string& _name) : Animal(_name) {}
 
-    // Implementation of the pure virtual function
+    // Overriding the base class's makeSound() function
     void makeSound() const override {
         std::cout << "Dog " << name << " says woof!" << std::endl;
     }
@@ -63,9 +62,20 @@ public:
         std::cout << "Hello, my name is " << name << "." << std::endl;
     }
 
-    void walkDog() const {
+     // Overloading the walkDog() function
+    void walkDog() {
         std::cout << name << " is walking his dog " << dog.getName() << "." << std::endl;
         dog.makeSound();
+    }
+
+    // Overloading the walkDog() function with a const-qualified version
+    void walkDog() const {
+        std::cout << name << " is taking a stroll with " << dog.getName() << "." << std::endl;
+    }
+
+    // Destructor
+    ~Person() {
+        std::cout << "Person destructor called for " << name << std::endl;
     }
 };
 
@@ -92,9 +102,7 @@ int main() {
     
     Dog dog1("Buddy");
     Person person("John", dog1);
-
     person.introduce();
-
     person.walkDog();
 
     Dog* dog2 = new Dog("Max");
@@ -102,26 +110,65 @@ int main() {
 
     Zoo zoo;
     zoo.addAnimal(animal);
-
     zoo.makeAnimalSounds();
 
-    delete dog2;
+    const Person constPerson("Alice", dog1);
+    // Invoking the const version of walkDog()
+    constPerson.walkDog();
 
+    delete dog2;
     return 0;
 
     /* 
-        Encapsulation: Data members are encapsulated within 
-        classes using access specifiers (protected and private).
-        Abstraction: The Animal class with a pure virtual function 
-        provides an abstraction for different types of animals.
-        The Dog objects dog1 and dog2 are regular objects with automatic storage duration. 
-        When the program execution reaches the end of the main function, the destructors for 
-        dog1 and dog2 are automatically invoked in reverse order of their creation.
-        Since the Dog class inherits from the Animal class, 
-        the Dog destructor is called first, and then the Animal destructor is 
-        automatically invoked as part of the destruction process.
+        Classes:
+        Animal: Base abstract class representing an animal. 
+        It has a protected member variable name and a pure virtual function makeSound().
+        Dog: Derived class from Animal. It has a constructor, a member function makeSound(), 
+        a member function getName(), and a destructor.
+        Person: Class representing a person. It has a private member variable name, a composition 
+        relationshipwith Dog through a private member variable dog, a constructor, and two member functions 
+        introduce() and walkDog().
+        Zoo: Class representing a zoo. It has a private member variable animals, which is a vector of pointers
+        to const Animal, demonstrating aggregation. It has member functions to add animals to the collection 
+        and make sounds of all animals.
+
+        Objects:
+        dog1: An object of the Dog class representing a dog named "Buddy".
+        person: An object of the Person class representing a person named "John" with a 
+        dog object dog1 as a member.
+        zoo: An object of the Zoo class representing a zoo. It contains a collection of animal objects.
+
+        Inheritance: The Dog class inherits from the Animal class using the 
+        public "access specifier". This relationship is established through the line class Dog : public Animal.
+
+        Encapsulation: Encapsulation is the mechanism of bundling data and methods that operate on that 
+        data within a class, hiding the internal implementation details and providing controlled access to 
+        the data. In the given code, encapsulation is achieved as follows:
+        The member variables name in the Animal class and dog in the Person class are declared as protected 
+        access specifiers. This means that they are accessible within the class and its derived classes but not 
+        from outside the class, promoting encapsulation by encapsulating the data within the class hierarchy.
+        The member variables name in the Animal class and Person class are not directly accessible from the 
+        main function or any other external code. Instead, access to these member variables is provided 
+        through appropriate member functions (getName() in the Dog class) and member functions 
+        (introduce() and walkDog()) in the Person class. This encapsulation ensures that the internal data is 
+        accessed and modified in a controlled manner, preserving data integrity and providing a level of abstraction.
+
+        Abstraction: Abstraction is the process of hiding the complex implementation details and exposing only
+        the essential features or behavior to the outside world. In the given code, abstraction is achieved 
+        through the use of an abstract base class:
+        The Animal class is an abstract base class that defines a pure virtual function makeSound(). 
+        By having a pure virtual function, the Animal class becomes an abstract class, which means it cannot 
+        be instantiated directly. It serves as a blueprint or interface for deriving concrete animal classes, 
+        such as the Dog class.
+        The Animal class provides an abstraction for different types of animals by declaring the makeSound() 
+        function as a pure virtual function. This abstracts the behavior of making a sound, allowing derived 
+        classes to provide their own implementation. This way, the client code can interact with different 
+        animal objects using a common interface without worrying about the specific details of each animal's 
+        sound.
+
         The virtual functions play a crucial role in achieving polymorphism and dynamic dispatch, 
         which are essential concepts in object-oriented programming.
+
         Polymorphism: Polymorphism allows objects of different classes to be treated as objects of a 
         common base class, enabling code reuse and flexibility. In the code, the Animal class has a 
         pure virtual function makeSound(), which makes it an abstract base class. The Dog class derives 
@@ -129,6 +176,7 @@ int main() {
         in the base class and providing an implementation in the derived class, we achieve polymorphism. 
         This allows objects of Dog to be treated as objects of Animal, providing a uniform interface 
         for different types of animals.
+
         Dynamic Dispatch: Dynamic dispatch enables the selection of the appropriate function implementation 
         at runtime based on the actual object type rather than the declared type. In the code, the 
         makeSound() function is declared as virtual in the Animal class and overridden in the Dog class. 
@@ -136,5 +184,47 @@ int main() {
         (e.g., Animal* animal = &dog2;), dynamic dispatch ensures that the correct implementation of the 
         virtual function is called based on the actual object type. This allows for runtime polymorphism, where 
         the appropriate makeSound() function of the specific animal type (e.g., Dog in this case) is invoked.
+
+        Composition: Composition represents a "has-a" relationship, where a class contains objects of other
+        classes as member variables, and the lifetime of the contained objects is tightly coupled with the 
+        containing object. In the code, the Person class demonstrates composition with the Dog class.
+        Inside the Person class, there is a member variable dog of type Dog. This means that a Person object 
+        has a Dog object as a part of its state. The Dog object is created and destroyed along with the 
+        Person object. The Dog object is initialized in the Person constructor using the provided Dog object 
+        as an argument. This demonstrates composition, where the Person class composes a Dog object.
+
+        Aggregation: Aggregation represents a "has-a" relationship as well, where a class contains objects of 
+        other classes as member variables, but the lifetime of the contained objects is independent of the 
+        containing object. In the code, the Zoo class demonstrates aggregation with the Animal class.
+        Inside the Zoo class, there is a vector animals of type const Animal*. This vector holds pointers to 
+        Animal objects, but the Zoo class doesn't have ownership or responsibility for creating or destroying 
+        those Animal objects. It is an aggregation because the Zoo class holds a collection of Animal objects,
+        but the lifetime of the Animal objects is managed externally.
+
+        The Dog objects dog1 and dog2 are regular objects with automatic storage duration. 
+        When the program execution reaches the end of the main function, the destructors for 
+        dog1 and dog2 are automatically invoked in reverse order of their creation.
+        Since the Dog class inherits from the Animal class, 
+        the Dog destructor is called first, and then the Animal destructor is 
+        automatically invoked as part of the destruction process.
+
+        The destructor ~Person() is added to the Person class. Inside the destructor, you can include any 
+        cleanup or release logic specific to the Person class. It simply outputs a message indicating that 
+        the destructor is called for a particular Person object.
+        The Person class in the provided code does not required a destructor explicitly defined.
+        It's important to note that in this case, since the Person class does not own the Dog object and 
+        is not responsible for its destruction, there is no need to explicitly handle the destruction 
+        of the Dog object within the Person destructor. The destruction of the Dog object will be 
+        automatically handled by the destructor of the Dog class or by the code that created the Dog object.
+        A destructor is typically necessary when a class manages resources that need to be explicitly 
+        released or cleaned up when an object of that class is destroyed. For example, if the Person class 
+        had dynamically allocated memory or owned resources that need to be released a destructor would be 
+        required to handle the cleanup. The Person class does not own any resources that require explicit 
+        cleanup. It has a composition relationship with a Dog object, but the responsibility for managing the 
+        Dog object's memory lies outside the Person class. The Dog object is either passed as a 
+        reference or created as a local object in the main() function, and its destruction is automatically 
+        handled when it goes out of scope or when explicitly deleted. Since the Person class does not 
+        require any additional cleanup beyond what is automatically handled for it's member variables, it 
+        does not need a destructor. The compiler-generated default destructor will be sufficient in this case.
     */
 }
